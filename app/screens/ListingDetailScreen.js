64d278fa6b248f7f408d94414630ac0791265e27
dylanbatar <dylanbatar2@gmail.com>
2020-10-27 15:05:16 -0500

    .

--- app/screens/ListingDetailScreen.js
+++ app/screens/ListingDetailScreen.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native-expo-image-cache';
-import { colors } from '../config/colors';
-import ListItemWithIcon from '../components/List/ListItemWithIcon';
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native-expo-image-cache";
+import { colors } from "../config/colors";
+import ListItemWithIcon from "../components/List/ListItemWithIcon";
 
 export default function ListingDetailScreen({ route }) {
   const { item } = route.params;
@@ -13,13 +13,13 @@ export default function ListingDetailScreen({ route }) {
       <Image style={styles.image} uri={item.images[0].url} />
       <View style={styles.descripcion}>
         <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.subtitle}>{'$' + item.price}</Text>
+        <Text style={styles.subtitle}>{"$" + item.price}</Text>
       </View>
       <View style={styles.profile}>
         <ListItemWithIcon
-          image={require('../assets/mosh.jpg')}
-          title='Dylan Batista'
-          subtitle='5 Listings'
+          image={require("../assets/mosh.jpg")}
+          title="Dylan Batista"
+          subtitle="5 Listings"
           size={60}
         />
       </View>
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 300,
   },
   descripcion: {
